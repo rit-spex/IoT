e9ba612ad659e3370c5d176ea2e027bcd89df37d
T.J. Tarazevits <tarazevits@gmail.com>
2017-05-05 06:01:28 -0400

    Successfully writing data to the server!!!

--- network-gateway/ng-serial-monitor.py
+++ network-gateway/ng-serial-monitor.py
@@ -1,6 +1,7 @@
 import serial
 import io
 from datetime import datetime
+import time
 import json
 from collections import deque
 from multiprocessing import Pool
@@ -8,20 +9,24 @@ import threading
 from socketIO_client import SocketIO, LoggingNamespace
 
 SERIAL_PORT = 'COM6'
+sockets = {}
+
+def sendDataViaSocket(socketData):
+  socketId = socketData['UUID']
+  if sockets.has_key(socketId):
+      socket = sockets.get(socketId)
+      sendSocketData(socket, socketData)
+  else:
+      sockets[socketId] = connectToSocket(socketId)
 
 def connectToSocket(socketId):
-    socketIO = SocketIO('localhost', 3000)
-    socketIO.on('joinedSuccessfully', addName)
-    socketIO.on('connect', joinServer)
-    socketIO.on('disconnect', disconnectFromServer)
-
     def on_response(data):
       print('response received: ')
       print(data)
 
     def joinServer(*args):
         print('connecting to HABnet server')
-        socketIO.emit('join', { 'name': 'pythonClient2', 'type': 'dataSource' })
+        socketIO.emit('join', {'name': 'pythonClient2', 'type': 'dataSource'})
 
     def disconnectFromServer(*args):
         print('disconnecting from HABnet server')
@@ -29,26 +34,44 @@ def connectToSocket(socketId):
     def addName(*args):
       print('join callback')
 
-    def sendSocketData(payload):
-      socketIO.emit('sensorData', {'dateCreated': '999', 'payload': {'python': 4}, 'name': 'pythonClient2'}, on_response)
+    socketIO = SocketIO('localhost', 3000)
+    socketIO.emit('join', {'name': socketId, 'type': 'dataSource'})
+    socketIO.on('joinedSuccessfully', addName)
+    socketIO.on('connect', joinServer)
+    socketIO.on('disconnect', disconnectFromServer)
+    return socketIO
+
+
+
+def sendSocketData(socket, payload):
+    message = {
+      'dateCreated': time.time(),
+      'name': payload['UUID'],
+      'payload': payload
+    }
+    socket.emit('sensorData', json.dumps(message))
+    print 'data sent'
+    #print json.dumps(message)
 
 def processSerial(inputString):
-    print(hello)
     start = hello.find('{')
     end = hello.find('}') + 1
     if (start > -1) and (end > -1):
         data = hello[start:end]
         d = json.loads(data)
-        print d["UUID"]
+        sendDataViaSocket(d)
 
+def workThroughQueue():
+  while True:
+      if len(serialQueue) > 0:
+          threading.Thread(target=processSerial, args=(serialQueue.popleft(),)).run()
+      
 ser = serial.Serial(SERIAL_PORT, 115200, timeout=0)
-sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+#sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
 serialQueue = deque([])
+threading.Thread(target=workThroughQueue).start()
 while True:
-    hello = sio.readline()
+    #hello = sio.readline()
+    hello = ser.readline()
     if hello != '':
        serialQueue.append(hello)
-
-    if len(serialQueue) > 0:
-        threading.Thread(target=processSerial, args=(serialQueue.popleft(),)).run()
-        
