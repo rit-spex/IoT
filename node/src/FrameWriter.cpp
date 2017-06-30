c444662390dc1c8acaaa65a9ddacaa32bbfb1647
Austin Bodzas <austinbodzas@gmail.com>
2017-06-29 23:38:11 -0400

    added escape checking

--- node/src/FrameWriter.cpp
+++ node/src/FrameWriter.cpp
@@ -1,20 +1,49 @@
 #include "FrameWriter.hpp"
-#include <cstdint>
 
 using std::uint8_t;
+using std::uint16_t;
 
 namespace {
+  constexpr uint16_t LEN = 256;
   constexpr uint8_t STX = 0x01;
   constexpr uint8_t ETX = 0x03;
   constexpr uint8_t ESC = 0xfe;
 }
 namespace SensorNode {
 
-void FrameWriter::sendMsg() {
-  int8_t buffer[256] = {0};
+void FrameWriter::sendMsg(uint8_t *vals, uint16_t length) {
+  uint16_t cursor = 0;
 
-  buffer[0] = ESC;
-  buffer[1] = STX;
+  _buffer[cursor] = ESC;
+  cursor++;
+  _buffer[cursor] = STX;
+  cursor++;
+
+  for(uint16_t i = 0; i < length; i++) {
+    if(_valNeedsEscape(vals[i])) {
+      _buffer[cursor] = ESC;
+      cursor++;
+    }
+
+    _buffer[cursor] = vals[i];
+    cursor++;
+  }
+
+  _buffer[cursor] = ESC;
+  cursor++;
+  _buffer[cursor] = ETX;
+  cursor++;
+}
+
+bool FrameWriter::_valNeedsEscape(uint8_t val) {
+  switch(val) {
+    case 0x01:
+    case 0x03:
+    case 0xfe:
+      return true;
+    default:
+      return false;
+  }
 }
 
 }
--- node/src/FrameWriter.hpp
+++ node/src/FrameWriter.hpp
@@ -2,6 +2,8 @@
 
 #include <RH_RF95.h>
 
+#include <cstdint>
+
 
 namespace SensorNode {
 
@@ -9,10 +11,13 @@ class FrameWriter {
  public:
   FrameWriter() {};
   ~FrameWriter() {};
-  void sendMsg();
+  void sendMsg(std::uint8_t *vals, std::uint16_t length);
 
  private:
+  bool _valNeedsEscape(uint8_t val);
+
   RH_RF95 _radio;
+  std::uint8_t _buffer[256] = {0};
 };
 
 } // end namespace RH_RF95
