294efad5bbca5daf99067aa556d77d44f83e3c2e
Austin Beau Bodzas <abb6499@rit.edu>
2017-06-30 16:13:58 -0400

    Abstracting frame writing to the buffer

--- node/src/FrameWriter.cpp
+++ node/src/FrameWriter.cpp
@@ -4,6 +4,13 @@ using std::uint8_t;
 using std::uint16_t;
 
 namespace {
+  // Radio setup, Adafruit LoRa feather
+  constexpr uint8_t RFM95_CS = 8;
+  constexpr uint8_t RFM95_RST = 4;
+  constexpr uint8_t RFM95_INT = 3;
+  constexpr float   RFM95_FREQ = 915.0; // MHz
+
+  // Message constants
   constexpr uint16_t LEN = 256;
   constexpr uint8_t STX = 0x01;
   constexpr uint8_t ETX = 0x03;
@@ -11,28 +18,47 @@ namespace {
 }
 namespace SensorNode {
 
+FrameWriter::FrameWriter() :
+    _radio(RH_RF95(RFM95_CS, RFM95_INT)) {}
+
 void FrameWriter::sendMsg(uint8_t *vals, uint16_t length) {
-  uint16_t cursor = 0;
+  _setUpBuffer(vals, length);
 
-  _buffer[cursor] = ESC;
-  cursor++;
-  _buffer[cursor] = STX;
-  cursor++;
+  if(!_isSetup)
+    _setup();
 
-  for(uint16_t i = 0; i < length; i++) {
-    if(_valNeedsEscape(vals[i])) {
-      _buffer[cursor] = ESC;
-      cursor++;
-    }
+  _radio.send(_buffer, _cursor);
+}
 
-    _buffer[cursor] = vals[i];
-    cursor++;
+void FrameWriter::_setup() {
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(100);
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!_radio.init())
+  {
+    while (1);
   }
 
-  _buffer[cursor] = ESC;
-  cursor++;
-  _buffer[cursor] = ETX;
-  cursor++;
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  _radio.setFrequency(RFM95_FREQ);
+  _radio.setModemConfig(RH_RF95::ModemConfigChoice::Bw500Cr45Sf128);
+  _radio.setTxPower(23, false);
+}
+
+void FrameWriter::_setUpBuffer(uint8_t *vals, uint16_t length) {
+  _addToBuffer(STX);
+
+  for(uint16_t i = 0; i < length; i++) {
+    _addMessageByte(vals[i]);
+  }
+
+  _addToBuffer(ETX);
 }
 
 bool FrameWriter::_valNeedsEscape(uint8_t val) {
@@ -43,6 +69,20 @@ bool FrameWriter::_valNeedsEscape(uint8_t val) {
       return true;
     default:
       return false;
+  }
+}
+
+void FrameWriter::_addMessageByte(uint8_t val) {
+  if(_valNeedsEscape(val))
+    _addToBuffer(ESC);
+
+  _addToBuffer(val);
+}
+
+void FrameWriter::_addToBuffer(uint8_t val) {
+  if(_cursor < LEN) {
+    _buffer[_cursor] = val;
+    _cursor++;
   }
 }
 
--- node/src/FrameWriter.hpp
+++ node/src/FrameWriter.hpp
@@ -9,15 +9,21 @@ namespace SensorNode {
 
 class FrameWriter {
  public:
-  FrameWriter() {};
+  FrameWriter();
   ~FrameWriter() {};
   void sendMsg(std::uint8_t *vals, std::uint16_t length);
 
  private:
-  bool _valNeedsEscape(uint8_t val);
+  bool _valNeedsEscape(std::uint8_t val);
+  void _addMessageByte(std::uint8_t val);
+  void _addToBuffer(std::uint8_t val);
+  void _setUpBuffer(std::uint8_t *vals, std::uint16_t length);
+  void _setup();
 
   RH_RF95 _radio;
   std::uint8_t _buffer[256] = {0};
+  std::uint16_t _cursor = 0;
+  bool _isSetup = false;
 };
 
 } // end namespace RH_RF95
