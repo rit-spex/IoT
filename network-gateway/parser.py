a10e3f1fd31bcf023c6aba03e86dda195b38b6ca
Austin Bodzas <abb6499@rit.edu>
2017-09-07 11:58:07 -0400

    parsing based on schema started

--- network-gateway/parser.py
+++ network-gateway/parser.py
@@ -2,6 +2,10 @@ import serial
 
 END = b'\xc0'
 
+BYTE = "byte"
+INT = "int"
+FLOAT = "float"
+
 class Scanner(object):
     def __init__(self, port):
         self._port = port
@@ -27,3 +31,29 @@ class Scanner(object):
     def deslip(self, buffer):
         buffer.replace(b'\xdb\xdc', b'\xc0')
         buffer.replace(b'\xdb\xdd', b'\xdb')
+
+class Parser(object):
+    def __init__(self, schema):
+        self._schema = schema
+
+
+    def change_scheme(self, new_schema):
+        self._schema = schema
+
+    def parse(self, buffer):
+        """Parse a buffer into a dict using a schema."""
+        offset = 0
+        ret_dict = {}
+
+        for tup in self._schema:
+            if tup[1] == FLOAT:
+                ret_dict[tup[0]] = struct.unpack('>f', buffer[offset:offset+4])
+                offset += 4
+            elif tup[1] == INT:
+                ret_dict[tup[0]] = struct.unpack('>i', buffer[offset:offset+4])
+                offset += 4
+            elif tup[1] == BYTE:
+                ret_dict[tup[0]] = struct.unpack('>B', buffer[offset:offset+1])
+                offset += 1
+
+        return ret_dict
